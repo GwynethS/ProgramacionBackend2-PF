c13fdc142904cb36f0b801f88ba4be5e94a6ce09
GwynethS <102232553+GwynethS@users.noreply.github.com>
2025-02-14 22:26:16 -0500

    user model edited

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,19 +3,36 @@ import mongoose from "mongoose";
 const userCollection = "users";
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+	cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Carts",
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "user",
+  },
 });
 
 const userModel = mongoose.model(userCollection, userSchema);
